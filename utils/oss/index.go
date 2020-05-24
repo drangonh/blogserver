a2a5d40ba8dabeeec8c3a25f424b6abf77416c29
786676631@qq.com <786676631@qq.com>
2020-05-24 08:52:54 +0800

    oss 图片上传封装

--- utils/oss/index.go
+++ utils/oss/index.go
@@ -24,15 +24,15 @@ func init() {
 * 初始化oss
  */
 func newClient() {
-	// Endpoint以杭州为例，其它Region请按实际情况填写。
-	endpoint = "https://oss-cn-shanghai.aliyuncs.com"
+	// Endpoint以杭州为例，其它Region请按实际情况填写。https://oss-cn-shanghai.aliyuncs.com
+	endpoint = ""
 
 	// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，
 	//请登录 https://ram.console.aliyun.com 创建RAM账号。
 
-	accessKeyId = "LTAI4GGxeHkKA7j7iXdCPzwL"
+	accessKeyId = ""
 
-	accessKeySecret = "XATCi8qAiF5bLRU5cl9doGlPoz7V6T"
+	accessKeySecret = ""
 
 	BucketName = "blogdrag"
 
